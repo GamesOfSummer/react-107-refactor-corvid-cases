4d74caffe16bd2978cd0e6a927958f33914be6c7
Summer <summer.d.howard@gmail.com>
2020-04-01 12:42:34 -0500

    converted card to a functional component

--- src/Card.tsx
+++ src/Card.tsx
@@ -4,30 +4,31 @@ import { state } from './types';
 
 interface CardProps { state: state; }
 
-export default class Card extends Component<CardProps>{
+const Card = (props: CardProps) => {
 
-    style = {
+    const style = {
         card: {
-            fontSize: 16,  
+            fontSize: 16,
             bottom: 0,
             left: 16,
             textAlign: 'left'
         }
     };
 
-    render() {
-        return (
-            <div
-           >
-                <div className="h-100 d-inline-block align-text-bottom">
-                    <div
-                        style={this.style.card as any}>
-                        <strong>{this.props.state.state}</strong><br />
-                        {this.props.state.case}
-                        {' '} <MDBIcon icon="adjust" />
-                    </div>
+    return (
+        <div
+        >
+            <div className="h-100 d-inline-block align-text-bottom">
+                <div
+                    style={style.card as any}>
+                    <strong>{props.state.state}</strong><br />
+                    {props.state.case}
+                    {' '} <MDBIcon icon="adjust" />
                 </div>
-            </div >
-        );
-    }
+            </div>
+        </div >
+    );
+
 }
+
+export default Card;
